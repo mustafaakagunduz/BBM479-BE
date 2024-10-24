fa9e8a2094165a70c201c1d7af953144740fd8a6
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2024-10-24 20:34:39 +0300

    Initial commit for Spring Boot project

---
+++ src/test/java/com/sms/hrsam/HrsamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sms.hrsam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrsamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
