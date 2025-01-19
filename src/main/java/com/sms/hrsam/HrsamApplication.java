9426a67d5067f2e8cc261e704e38ff7928648f79
selhatinenc <selhattinenc@hacettepe.edu.tr>
2025-01-19 22:27:58 +0300

    app.prop and pom.xml changed

--- src/main/java/com/sms/hrsam/HrsamApplication.java
+++ src/main/java/com/sms/hrsam/HrsamApplication.java
@@ -1,13 +1,32 @@
 package com.sms.hrsam;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class HrsamApplication {
 
+	@Autowired
+	private DataSource dataSource;
+
+	@PostConstruct
+	public void checkDatabaseConnection() {
+		try (Connection conn = dataSource.getConnection()) {
+			System.out.println("Database connection successful!");
+			System.out.println("Database: " + conn.getCatalog());
+		} catch (SQLException e) {
+			System.err.println("Database connection failed!");
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HrsamApplication.class, args);
 	}
-	//çay molası
 }
